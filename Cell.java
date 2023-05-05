524cd29135a968ca37867dbd8045e24495753e7a
MJR115 <mj.ksa99@hotmail.com>
2023-05-05 19:57:16 -0400

    ci

--- Cell.java
+++ Cell.java
@@ -1,63 +1,64 @@
 public class Cell {
-    // instance variables
-    private boolean hasMine;        // does the cell contain a mine?
-    private boolean isEmpty;        // is the cell empty?
-    private boolean isFlagged;      // OPTIONAL - is the cell flagged?
-    private int numMines;           // how many mines surround the cell?
-    private boolean isRevealed;     // has the cell been revealed to the user?
-    private char content;           // what to print to the screen for the cell
+    public class Cell {
+        // instance variables
+        private boolean hasMine;        // does the cell contain a mine?
+        private boolean isEmpty;        // is the cell empty?
+        private boolean isFlagged;      // OPTIONAL - is the cell flagged?
+        private int numMines;           // how many mines surround the cell?
+        private boolean isRevealed;     // has the cell been revealed to the user?
+        private char content;           // what to print to the screen for the cell
 
-    /** constructors (at least 3): creates a cell and initializes instance variables **/
-    // FIXME - UNFINISHED: create cell and add more constructors
-    public Cell() {
-        // create a cell
-        hasMine = false;
-        isEmpty = false;
-        isFlagged = false;
-        numMines = 0;
-        isRevealed = false;
-        content = '#';
-    }
+        /* constructors (at least 3): creates a cell and initializes instance variables */
+        // FIXME - UNFINISHED: create cell and add more constructors
+        public Cell() {
+            // create a cell
+            hasMine = false;
+            isEmpty = false;
+            isFlagged = false;
+            numMines = 0;
+            isRevealed = false;
+            content = '#';
+        }
 
-    /** methods: get, set, & other access methods **/
-    // FIXME - UNFINISHED: other access methods
-    /** set methods **/
-    public void setHasMine(boolean m) {
-        hasMine = m;
-    }
-    public void setIsEmpty(boolean e){
-        isEmpty = e;
-    }
-    public void setIsFlagged(boolean f) {
-        isFlagged = f;
-    }
-    public void setNumMines(int m){
-        numMines = m;
-    }
-    public void setIsRevealed(boolean r){
-        isRevealed = r;
-    }
-    public void setContent(char c){
-        content = c;
-    }
+        // methods: get, set, & other access methods /
+        // FIXME - UNFINISHED: other access methods
+        // set methods /
+        public void setHasMine(boolean m) {
+            hasMine = m;
+        }
+        public void setIsEmpty(boolean e){
+            isEmpty = e;
+        }
+        public void setIsFlagged(boolean f) {
+            isFlagged = f;
+        }
+        public void setNumMines(int m){
+            numMines = m;
+        }
+        public void setIsRevealed(boolean r){
+            isRevealed = r;
+        }
+        public void setContent(char c){
+            content = c;
+        }
 
-    /** get methods **/
-    public boolean getHasMine() {
-        return hasMine;
+        // get methods /
+        public boolean getHasMine() {
+            return hasMine;
+        }
+        public boolean getIsEmpty(){
+            return isEmpty;
+        }
+        public boolean getIsFlagged() {
+            return isFlagged;
+        }
+        public int getNumMines(){
+            return numMines;
+        }
+        public boolean getIsRevealed(){
+            return isRevealed;
+        }
+        public char getContent(){
+            return content;
+        }
     }
-    public boolean getIsEmpty(){
-        return isEmpty;
-    }
-    public boolean getIsFlagged() {
-        return isFlagged;
-    }
-    public int getNumMines(){
-        return numMines;
-    }
-    public boolean getIsRevealed(){
-        return isRevealed;
-    }
-    public char getContent(){
-        return content;
-    }
-}
--- Minesweeper.java
+++ Minesweeper.java
@@ -1,27 +1,37 @@
 import java.util.Random;
-
 public class Minesweeper {
 
     public int dimX;
     public int dimY;
 
-    // public Cell[][] gameBoard(int x, int y) { }
+    public Minesweeper(int x, int y) {
 
-    public char[ ][ ] gameBoard(int x, int y) {
+        dimX = x;
+        dimY = y;
 
-        char[][] cell = new char[8][8];
+    }
 
+    public Cell[][] gameBoard(int x, int y) {
+
+        Cell[][] cell = new Cell[dimX][dimY];
+        for (int i = 0; i < dimX; i++) {
+            for (int j = 0; j < dimY; j++) {
+
+                cell[i][j] = new Cell();
+
+            }
+        }
         Random rand = new Random();
         int randMines = rand.nextInt(y - x + 1) + x;
 
 
         for (int i = 0; i < randMines; i++) {
             boolean check = false;
-            while(check == false) {
-                int randCol = rand.nextInt(8);
-                int randRows = rand.nextInt(8);
-                if(cell[randRows][randCol] != '*') {
-                    cell[randRows][randCol] = '*';
+            while (check == false) {
+                int randCol = rand.nextInt(dimX);
+                int randRows = rand.nextInt(dimY);
+                if (cell[randRows][randCol].getHasMine() == false) {
+                    cell[randRows][randCol].setHasMine(true);
                     check = true;
                 }
             }
@@ -29,61 +39,67 @@ public class Minesweeper {
         }
         return cell;
     }
-    // public void countAdjacentMines(CellFirstname[][] game) {}
-    public void countAdjacentMines(char[][] a) {
+
+    public void countAdjacentMines(Cell[][] a) {
         int numMines = 0;
         int colum = 0;
         int row = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++){
-                if (a[i][j] == '+'){
-                    row = i;
-                    colum = j;
-                }
-            }
-        }
+        for (int i = 0; i < dimX; i++) {
+            for (int j = 0; j < dimY; j++) {
 
-        for (int i = (row - 1); i <= (row + 1); i ++) {
-            for (int j = (colum - 1); j <= (colum + 1); j ++){
-                if (i >= 0 && i <= 7 ){
-                    if (j >= 0 && j <= 7) {
-                        if (a[i][j] == '*')
-                            numMines += 1;
+                for (int k = (i - 1); i <= (i + 1); k++) {
+                    for (int p = (j - 1); j <= (j + 1); p++) {
+                        if (i >= 0 && i <= dimX - 1) {
+                            if (j >= 0 && j <= dimY - 1) {
+                                if (a[k][p].getHasMine() == true)
+                                    a[k][p].setNumMines(a[k][p].getNumMines() + 1);
+                            }
+                        }
                     }
                 }
             }
         }
 
-
-
-
     }
-    // void setAllContents(Cell*[ ][ ] game) {}
-    public void setAllContents(char[][] a) {
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; i < 8; j++) {
+    public void setAllContents(Cell[][] a) {
+    }
 
-                //if(a[i][j] == '')
+    public void printBoard(Cell[][] a, boolean m) {
+
+        if (m == false) {
+            for (int i = 0; i < dimX; i++) {
+                for (int j = 0; j < dimY; j++) {
+
+                    if (a[i][j].getIsFlagged() == true) {
+                        System.out.print("F");
+                    } else if (a[i][j].getIsRevealed() == false) {
+                        System.out.print("#");
+                    } else {
+                        System.out.print(a[i][j].getNumMines());
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < dimX; i++) {
+                for (int j = 0; j < dimY; j++) {
+                    if (a[i][j].getHasMine() == true) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(a[i][j].getNumMines());
+                    }
+                }
             }
         }
     }
 
-    // void printBoard(Cell*[ ][ ] game, boolean mine) {}
+    public boolean checkWinner(Cell[][] a) {
+        boolean check = false;
 
-    /* public void play(CellFirstname[][] ) {
-          ask the user for row and column number
-          if they enter an incorrect cell, ask them again
 
-          if they stepped on a mine - the game is over
-          print the board
 
-          if they stepped on somewhere else
-          print the board
+        return check;
     }
-    */
-
-
 
 
 }
