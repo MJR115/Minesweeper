3ef4d6c07e481d9a856d8ccf5e36ce657f52264f
oboeMaster1 <132633256+oboeMaster1@users.noreply.github.com>
2023-05-05 13:19:09 -0400

    Complete Test template

--- Test.java
+++ Test.java
@@ -1,2 +1,31 @@
 public class Test {
+
 }
+
+// Here is how this class should be when we are done - until then it is for testing purposes
+
+/************************************************************
+ * Programming Assignment: Minesweeper
+ *
+ * TestYourname.java test the Minesweeper Game using corresponding
+ * Cell and Minesweeper classes
+ *
+ * YOU ARE NOT SUPPOSED TO MAKE ANY CHANGES TO THIS JAVA FILE
+ * EXCEPT CLASS NAMES.
+ *
+ * *********************************************************/
+/* public class TestYourname{ // what is your first name?
+
+    public static void main(String[] args){
+
+        MinesweeperYourname m = new MinesweeperYourname(10, 10); // Minesweeper instance: board dimension 10x10
+        CellFirstname[][] ms = m.gameBoard(8, 15); // initialize the board with number of mines between 8 and 15
+        m.setAllContents(ms); // set contents of each cell
+        m.printBoard(ms, false); // print the initial board
+        m.play(ms); // play the game
+
+        // YOU ARE NOT SUPPOSED TO MAKE ANY CHANGES TO THIS JAVA FILE
+        // EXCEPT CLASS NAMES.
+    }
+}
+*/
