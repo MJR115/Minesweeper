56e9071651d38a063b82640eb298d4fe4d4878b9
oboeMaster1 <132633256+oboeMaster1@users.noreply.github.com>
2023-05-07 21:06:40 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,17 @@
-import java.util.Scanner; // DEL
+/*  Assignment: PA4 Minesweeper
+    Minesweeper.java
+    Name: Genevieve Britten and Amjad Rabaan (group work)
+    Due May 8, 2023
+*/
 
 public class Test {
-  
-  public static void main(String[] args){
-    Scanner kb = new Scanner(System.in); // DEL
+
+    public static void main(String[] args){
       Minesweeper m = new Minesweeper(9, 9); // Minesweeper instance: board dimension 10x10
       Cell[][] ms = m.gameBoard(10, 13); // initialize the board with number of mines between 8 and 15
-    m.setAllContents(ms); // set contents of each cell
-    m.printBoard(ms, false); // print the initial board
-    m.play(ms); // play the game
+      m.setAllContents(ms); // set contents of each cell
+      m.printBoard(ms, false); // print the initial board
+      m.play(ms); // play the game
 
-  }
+    }
 }
